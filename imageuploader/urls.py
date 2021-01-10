d827bebbd9862585cc1fae6858be123c27b13a3e
Milan Jakhotra <milanjakhotra1234@gmail.com>
2021-01-10 13:03:32 +0530

    second

--- imageuploader/urls.py
+++ imageuploader/urls.py
@@ -6,5 +6,7 @@ from django.conf.urls.static import static
 from myapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home)
+    path('',views.home),
+       url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
